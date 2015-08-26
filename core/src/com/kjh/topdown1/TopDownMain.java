01565289fa0c3dfa1091c9339c3c5611fa08044f
Ken Horemans <ken.horemans@gmail.com>
2015-08-25 18:47:11 -0600

    Added plane rendering

--- core/src/com/kjh/topdown1/TopDownMain.java
+++ core/src/com/kjh/topdown1/TopDownMain.java
@@ -3,16 +3,22 @@ package com.kjh.topdown1;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class TopDownMain extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
 	BitmapFont font;
-	FPSLogger fpsLogger;
+	FPSLogger fpsLogger = new FPSLogger();
 	OrthographicCamera camera;
 	Texture background;
+	TextureRegion terrainBelow, terrainAbove;
+	float terrainOffset=0;
+	Animation plane;
+	float planeAnimTime=0;
 	
 	@Override
 	public void create () {
@@ -21,8 +27,16 @@ public class TopDownMain extends ApplicationAdapter {
 		font = new BitmapFont(Gdx.files.internal("verdana39.fnt"), Gdx.files.internal("verdana39.png"), false);
 		font.setColor(Color.PURPLE);
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false,800,400);
+		camera.setToOrtho(false,800,480);
 		background = new  Texture("background.png");
+		terrainBelow = new TextureRegion(new Texture("groundGrass.png"));
+		terrainAbove = new TextureRegion(terrainBelow);
+		terrainAbove.flip(true,true);
+		plane = new Animation (0.05f, new TextureRegion(new Texture("planeRed1.png")),
+									new TextureRegion(new Texture("planeRed2.png")),
+									new TextureRegion(new Texture("planeRed3.png")),
+									new TextureRegion(new Texture("planeRed2.png")));
+		plane.setPlayMode(Animation.PlayMode.LOOP);
 
 	}
 
@@ -47,9 +61,23 @@ public class TopDownMain extends ApplicationAdapter {
 	}
 
 	private void updateScene() {
-
+		float deltaTime = Gdx.graphics.getDeltaTime();
+		// try some scrolling! - terrainOffset -= 200*deltaTime;
+		planeAnimTime+=deltaTime;
 	}
 	private void drawScene() {
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.disableBlending();
+		batch.draw(background, 0, 0);
+		batch.enableBlending();
+		batch.draw(terrainBelow, terrainOffset, 0);
+		batch.draw(terrainBelow, terrainOffset + terrainBelow.getRegionWidth(), 0);
+		batch.draw(terrainAbove, terrainOffset, 480-terrainAbove.getRegionHeight());
+		batch.draw(terrainAbove, terrainOffset + terrainAbove.getRegionWidth(), 480-terrainAbove.getRegionHeight());
+		batch.draw(plane.getKeyFrame(planeAnimTime),350,200);
+		batch.end();
 
 	}
 }
--- desktop/src/com/kjh/topdown1/desktop/DesktopLauncher.java
+++ desktop/src/com/kjh/topdown1/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.kjh.topdown1.TopDownMain;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new TopDownMain(), config);
 	}
 }
