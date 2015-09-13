9dfc6ef25fa27559ad61aebc8a472f8a8885f4b0
Ken Horemans <ken.horemans@gmail.com>
2015-09-13 08:27:01 -0600

    Added pillars, basic collision detection touch graphics and basic sounds.

--- core/src/com/kjh/topdown1/TopDownMain.java
+++ core/src/com/kjh/topdown1/TopDownMain.java
@@ -1,12 +1,17 @@
 package com.kjh.topdown1;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -32,13 +37,45 @@ public class TopDownMain extends ApplicationAdapter {
 	Vector3 touchPosition = new Vector3();
 	Vector2 tmpVector = new Vector2();
 	private static final int TOUCH_IMPULSE = 500;
-	TextureRegion tapIndicator;
+	TextureRegion tapIndicator1, tapIndicator2;
 	float tapDrawTime;
 	private static final float TAP_DRAW_TIME_MAX=1.0f;
-	
+	Texture gameOver;
+	Vector2 scrollVelocity = new Vector2();
+	Array<Vector2> pillars = new Array<Vector2>();
+	Vector2 lastPillarPosition = new Vector2(0,0);
+	float deltaPosition;
+	TextureRegion pillarUp, pillarDown;
+	Rectangle planeRect = new Rectangle();
+	Rectangle obstacleRect = new Rectangle();
+	Music music;
+	Sound tapSound;
+	Sound crashSound;
+	Sound spawnSound;
+	// game state manager
+	static enum GameState {
+		INIT, ACTION, GAME_OVER
+	}
+	GameState gameState = GameState.INIT;
+
+
 	@Override
 	public void create () {
 		resetScene();
+		// for giggles lets try an alloc
+		class inner1 {
+			int blocktype;
+			int subtype;
+		}
+
+		inner1[][] worldarray = new inner1[1024][1024];
+		for (int a = 0 ;a<1024;a++) {
+ 			for (int b = 0; b < 1024; b++) {
+				inner1 i = worldarray[a][b] = new inner1();
+			}
+		}
+		Gdx.app.log("Val 7 7", Integer.toString(worldarray[7][7].blocktype));
+
 		batch = new SpriteBatch();
 		//img = new Texture("badlogic.jpg");
 		//font = new BitmapFont(Gdx.files.internal("verdana39.fnt"), Gdx.files.internal("verdana39.png"), false);
@@ -64,7 +101,18 @@ public class TopDownMain extends ApplicationAdapter {
 								//	new TextureRegion(new Texture("planeRed3.png")),
 								//	new TextureRegion(new Texture("planeRed2.png")));
 		plane.setPlayMode(Animation.PlayMode.LOOP);
-		tapIndicator=atlas.findRegion("tap2");
+		tapIndicator2=atlas.findRegion("tap2");
+		tapIndicator1 = atlas.findRegion("tap1");
+		gameOver = new Texture("gameover.png");
+		pillarUp = atlas.findRegion("rockGrassUp");
+		pillarDown = atlas.findRegion("rockGrassDown");
+
+		music = Gdx.audio.newMusic(Gdx.files.internal("sounds/journey.mp3"));
+		music.setLooping(true);
+		music.play();
+		tapSound = Gdx.audio.newSound(Gdx.files.internal("sounds/pop.ogg"));
+		crashSound = Gdx.audio.newSound(Gdx.files.internal("sounds/crash.ogg"));
+		spawnSound = Gdx.audio.newSound(Gdx.files.internal("sounds/alarm.ogg"));
 	}
 
 	@Override
@@ -88,18 +136,50 @@ public class TopDownMain extends ApplicationAdapter {
 	@Override
 	public void dispose() {
 		batch.dispose();
+		music.dispose();
+		pillars.clear();
+		atlas.dispose();
+		tapSound.dispose();
+		crashSound.dispose();
+		spawnSound.dispose();
 		//font.dispose();
 
 	}
 
 	private void updateScene() {
+
+		if (Gdx.input.justTouched()) {
+			tapSound.play();
+			if (gameState == GameState.INIT) {
+				gameState = GameState.ACTION;
+				return;
+			}
+			if (gameState == GameState.GAME_OVER) {
+				gameState = GameState.INIT;
+				resetScene();
+				return;
+			}
+			touchPosition.set(Gdx.input.getX(), Gdx.input.getY(),0);
+			camera.unproject(touchPosition); // go from screen to world coords
+			tmpVector.set(planePosition.x, planePosition.y);
+			tmpVector.sub(touchPosition.x, touchPosition.y).nor();
+			planeVelocity.mulAdd(tmpVector, TOUCH_IMPULSE- MathUtils.clamp(Vector2.dst(touchPosition.x, touchPosition.y, planePosition.x, planePosition.y),0,TOUCH_IMPULSE));
+			tapDrawTime = TAP_DRAW_TIME_MAX;
+		}
+
+		if (gameState == GameState.INIT || gameState == GameState.GAME_OVER) {
+			return;
+		}
 		float deltaTime = Gdx.graphics.getDeltaTime();
+		tapDrawTime -= deltaTime;
 		// try some scrolling! - terrainOffset -= 200*deltaTime;
 		planeAnimTime+=deltaTime;
 		planeVelocity.scl(damping);
 		planeVelocity.add(gravity); // add gravity as a vector to velocity
-		planeVelocity.add(5.0f,0);
+		planeVelocity.add(scrollVelocity);
 		planePosition.mulAdd(planeVelocity, deltaTime); //scalar multipy deltatime by velocity vector
+		deltaPosition = planePosition.x-planeDefaultPosition.x;
+
 		//Gdx.app.log("Velocity",planeVelocity.toString());
 		//Gdx.app.log("TerrainOffset", Float.toString(terrainOffset));
 		// shift the background, not the plane..
@@ -111,16 +191,42 @@ public class TopDownMain extends ApplicationAdapter {
 		if (terrainOffset > 0) {
 			terrainOffset =-terrainBelow.getRegionWidth();
 		}
-		if (Gdx.input.justTouched()) {
-			touchPosition.set(Gdx.input.getX(), Gdx.input.getY(),0);
-			camera.unproject(touchPosition); // go from screen to world coords
-			tmpVector.set(planePosition.x, planePosition.y);
-			tmpVector.sub(touchPosition.x, touchPosition.y).nor();
-			planeVelocity.mulAdd(tmpVector, TOUCH_IMPULSE- MathUtils.clamp(Vector2.dst(touchPosition.x, touchPosition.y, planePosition.x, planePosition.y),0,TOUCH_IMPULSE));
-			tapDrawTime = TAP_DRAW_TIME_MAX;
+		if ((planePosition.y < terrainBelow.getRegionHeight()-35) || (planePosition.y+73 > 480-terrainBelow.getRegionHeight()+35)) {
+			if (gameState != GameState.GAME_OVER) {
+				gameState = GameState.GAME_OVER;
+				crashSound.play();
+			}
 		}
-		tapDrawTime -= deltaTime;
 
+		planeRect.set(planePosition.x + 16, planePosition.y, 50, 73);
+		for(Vector2 vec: pillars)
+		{
+			vec.x-=deltaPosition;
+			if(vec.x+pillarUp.getRegionWidth()<-10)
+			{
+				pillars.removeValue(vec, false);
+			}
+			if(vec.y==1)
+			{
+				obstacleRect.set(vec.x + 10, 0, pillarUp.getRegionWidth()-20, pillarUp.getRegionHeight()-10);
+			}
+			else
+			{
+				obstacleRect.set(vec.x + 10, 480-pillarDown.getRegionHeight()+10, pillarUp.getRegionWidth()-20, pillarUp.getRegionHeight());
+			}
+			if(planeRect.overlaps(obstacleRect))
+			{
+				if(gameState != GameState.GAME_OVER)
+				{
+					gameState = GameState.GAME_OVER;
+					crashSound.play();
+				}
+			}
+		}
+		//Gdx.app.log("lpp",Float.toString(lastPillarPosition.x));
+		if (lastPillarPosition.x < 400) {
+			addPillar();
+		}
 	}
 	private void drawScene() {
 		camera.update();
@@ -130,14 +236,28 @@ public class TopDownMain extends ApplicationAdapter {
 		batch.disableBlending();
 		batch.draw(bgregion, 0, 0);
 		batch.enableBlending();
+		for (Vector2 v: pillars) {
+			if (v.y == 1) {
+				batch.draw(pillarUp, v.x, 0);
+			} else {
+				batch.draw(pillarDown, v.x, 480-pillarDown.getRegionHeight());
+			}
+		}
 		batch.draw(terrainBelow, terrainOffset, 0);
 		batch.draw(terrainBelow, terrainOffset + terrainBelow.getRegionWidth(), 0);
 		batch.draw(terrainAbove, terrainOffset, 480-terrainAbove.getRegionHeight());
 		batch.draw(terrainAbove, terrainOffset + terrainAbove.getRegionWidth(), 480-terrainAbove.getRegionHeight());
 		batch.draw(plane.getKeyFrame(planeAnimTime),planePosition.x, planePosition.y);
-		if (tapDrawTime>0) {
-			batch.draw(tapIndicator, touchPosition.x-29.5f, touchPosition.y-29.5f);
+		if (gameState == GameState.INIT) {
+			batch.draw(tapIndicator1, planePosition.x, planePosition.y-80);
 		}
+		if (tapDrawTime>0) {
+			batch.draw(tapIndicator2, touchPosition.x-29.5f, touchPosition.y-29.5f);
+		}
+		if (gameState == GameState.GAME_OVER) {
+			batch.draw(gameOver, 400-206, 240-80);
+		}
+
 		batch.end();
 
 	}
@@ -145,10 +265,29 @@ public class TopDownMain extends ApplicationAdapter {
 	private void resetScene() {
 		terrainOffset = 0;
 		planeAnimTime = 0;
-		planeVelocity.set(0,0);
-		gravity.set(0,-2);
-		planeDefaultPosition.set(400-88/2, 240-73/2); // center of image and screen
+		planeVelocity.set(0, 0);
+		gravity.set(0, -2);
+		planeDefaultPosition.set(400 - 88 / 2, 240 - 73 / 2); // center of image and screen
 		planePosition.set(planeDefaultPosition.x, planeDefaultPosition.y);
+		scrollVelocity.set(4, 0);
+
+	}
+
+	private void addPillar() {
+		Vector2 pillarPosition = new Vector2();
+		if (pillars.size == 0) {
+			pillarPosition.x=(float)(800+Math.random()*600);
+		} else {
+			pillarPosition.x = lastPillarPosition.x+(float) (600+Math.random()*600);
+		}
+		if (MathUtils.randomBoolean()) {
+			pillarPosition.y = 1;
+		} else {
+			pillarPosition.y = -1;
+		}
+		lastPillarPosition = pillarPosition;
+		pillars.add(pillarPosition);
+
 	}
 
 }
